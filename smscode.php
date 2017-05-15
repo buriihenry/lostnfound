147e776af26ebfcafd4247fdda47b853edccfd52
Burii <buriihenry@gmail.com>
2017-05-15 03:48:46 -0400

    Update smscode.php

--- smscode.php
+++ smscode.php
@@ -2,8 +2,8 @@
     // Be sure to include the file you've just downloaded
     require_once('AfricasTalkingGateway.php');
     // Specify your login credentials
-    $username   = "erickwarui28";
-    $apikey     = "3daefe2ecc9e8143e43187228f8a1d371bf7876aa455782315f1343066b21ce0";
+    $username   = "burii";
+    $apikey     = "95957b84b4ee39c5324d5a8c706a5763577468c3bbba71c8842925c954b94448";
     // Specify the numbers that you want to send to in a comma-separated list
     // Please ensure you include the country code (+254 for Kenya in this case)
     $recipients = "+254700223146";
