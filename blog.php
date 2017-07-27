6aae1891f0e0c4b308d107551f7f72a9577748c6
Burii <buriihenry@gmail.com>
2017-07-27 10:48:41 +0300

    Update blog.php

--- blog.php
+++ blog.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title></title>
+    <title>Blog</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
